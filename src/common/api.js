24e7b7e85e8b069c032a7b64b444cfa8994cec07
Sneh Jogani <sjogani16@gmail.com>
2020-11-16 21:45:11 -0400

    implemented openweatherapi

---
+++ src/common/api.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+import { toast } from 'react-toastify'
+
+const axiosInstance = axios.create({
+  baseURL: 'http://api.openweathermap.org/data/2.5',
+  params: {
+    appid: 'c51223c219d6aec8cb8c5210449bd859',
+    units: 'metric'
+  },
+})
+
+export const getCurrentWeatherByCity = (city, callback) => {
+  return axiosInstance.get('/weather', {
+    params: {
+      q: city,
+    }
+  })
+    .then(({ data }) => {
+      const { id, name, main: { temp }, weather: [weather] } = data
+      callback({ id, name, temp, weather })
+    })
+    .catch(err => {
+      if (err.response) {
+        toast.error(err.response.data.message[0].toUpperCase() + err.response.data.message.slice(1,) + '!')
+      }
+    })
+}
+
+export const getWeatherForecastByCity = (city, callback) => {
+  return axiosInstance.get('/forecast/daily', {
+    params: {
+      q: city,
+      cnt: 5
+    }
+  })
+    .then(({ data: { list } }) => {
+      callback(list)
+    })
+    .catch(err => {
+      if (err.response) {
+        toast.error(err.response.data.message[0].toUpperCase() + err.response.data.message.slice(1,) + '!')
+      }
+    })
+}

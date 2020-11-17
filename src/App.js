219c203376b11e920e084efac426909ab46756c9
Sneh Jogani <sjogani16@gmail.com>
2020-11-16 21:47:32 -0400

    view panes added to App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import { useState } from 'react'
-import { Card, Row, Col, Container } from 'react-bootstrap'
-import { toast, ToastContainer } from 'react-toastify'
+import { Row, Col, Container } from 'react-bootstrap'
+import { ToastContainer } from 'react-toastify'
 
-import './App.css';
-import SearchBox from './components/SearchBox'
-import { getWeatherByCity } from './common/api'
+import CityListView from './views/CityListView'
+import WeatherForecastView from './views/WeatherForecastView'
 
 function App() {
-  const [cityName, setCityName] = useState('')
-
-  const onCityNameChange = (event) => {
-    setCityName(event.target.value)
-  }
-
-  const onAddButtonClick = () => {
-    // console.log({ cityName })
-    getWeatherByCity(cityName)
-      .then(res => {
-        console.log(res)
-      })
-      .catch(err => {
-        if (err && err.data.cod === '404') {
-          toast.error(err.data.message[0].toUpperCase() + err.data.message.slice(1,) + '!')
-        }
-      })
-  }
+  const [selectedCity, setSelectedCity] = useState(null)
 
   return <div className="App">
-    <ToastContainer />
+    <ToastContainer newestOnTop />
     <Container fluid>
       <Row>
-        <Col>
-          <Card>
-            <SearchBox
-              onChange={onCityNameChange}
-              onSubmit={onAddButtonClick}
-            />
-          </Card>
+        <Col md={4}>
+          <CityListView setSelectedCity={setSelectedCity} />
         </Col>
         <Col>
+          {selectedCity !== null ? <WeatherForecastView selectedCity={selectedCity} /> : null}
         </Col>
       </Row>
     </Container>
